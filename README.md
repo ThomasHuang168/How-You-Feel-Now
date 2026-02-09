33666f9848476e704e47ce36f074327304a6045d
Thomas Huang Hing Pang <huanghingpang@gmail.com>
2026-02-09 20:53:24 +0000

    finetuning

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ a python script collect feeling (by where they are from) with timestamp
 
 ## Updates:
 
+20260209: finetuning
+
 20250531: hide responses and make response a csv
 
 20250530: Init repos, make button circular like traffic light, allow flexible postcode
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import PhotoImage
 import logging
 from pathlib import Path
 import re
 from datetime import datetime
+import webbrowser
 
 # Configure logging to write to responses.log with timestamp
 logPath:Path = Path("responses") / datetime.now().strftime("%Y%m%d.csv")
@@ -22,25 +24,29 @@ logging.basicConfig(filename=str(logPath), level=logging.INFO,
 #         log_text.insert(tk.END, "".join(logLines))
 #         log_text["state"] = tk.DISABLED
 
+def open_feedback_link():
+    webbrowser.open("https://docs.google.com/forms/d/e/1FAIpQLSdcFlKouCo0ckwc-HZeID-eVx6lqmJW9R4PkC_JynhWv25FEg/viewform")
+
 def save_response(feeling):
-    postcode = postcode_entry.get()
+    #postcode = postcode_entry.get()
 
-    if not postcode:
-        messagebox.showwarning("Input Error", "Please enter your postcode.")
-        return
-    elif not re.fullmatch(r'[A-Za-z]{1,2}[0-9R][0-9A-Za-z]? ?[0-9][A-Za-z]{2}', postcode):
-        messagebox.showwarning("Input Error", f"{postcode} is not a proper Postcode")
-        return
+    #if not postcode:
+    #    messagebox.showwarning("Input Error", "Please enter your postcode.")
+    #    return
+    #elif not re.fullmatch(r'[A-Za-z]{1,2}[0-9R][0-9A-Za-z]? ?[0-9][A-Za-z]{2}', postcode):
+    #    messagebox.showwarning("Input Error", f"{postcode} is not a proper Postcode")
+    #    return
 
-    postcode = postcode.upper()
-    postcode = re.sub(r'([A-Za-z]{1,2}[0-9R][0-9A-Za-z]?) ?([0-9][A-Za-z]{2})',r'\1 \2', postcode)
-    log_entry = f"{postcode}, {feeling}"
+    #postcode = postc ode.upper()
+    #postcode = re.sub(r'([A-Za-z]{1,2}[0-9R][0-9A-Za-z]?) ?([0-9][A-Za-z]{2})',r'\1 \2', postcode)
+    #log_entry = f"{postcode}, {feeling}"
+    log_entry = f"{feeling}"
     logging.info(log_entry)
 
     # show_responses()
     
     messagebox.showinfo("Success", "Response saved successfully!")
-    postcode_entry.delete(0, tk.END)
+    #postcode_entry.delete(0, tk.END)
 
 def create_circle_button(canvas:tk.Canvas, x, y, radius, color, text, command, text_color):
     """Creates a circular button using the Canvas widget"""
@@ -51,11 +57,16 @@ def create_circle_button(canvas:tk.Canvas, x, y, radius, color, text, command, t
                                 fill=color, 
                                 # outline="black",
                                 )
-    label = canvas.create_text(x, y, text=text, font=("Arial", 28), fill=text_color)
+
+    if text[-4:] != ".png":
+        label = canvas.create_text(x, y, text=text, font=("Arial", 28), fill=text_color)
+    else:
+        image = PhotoImage(file=text)
+        root.image=image
+        label = canvas.create_image(x, y, image=image)
 
     def on_click(event):
         command()
-
     # Bind click event to the circular button
     canvas.tag_bind(circle, "<Button-1>", on_click)
     canvas.tag_bind(label, "<Button-1>", on_click)
@@ -68,10 +79,10 @@ root.attributes('-fullscreen', True)  # Enable fullscreen
 # Exit fullscreen with Escape key
 root.bind("<Escape>", lambda event: root.attributes('-fullscreen', False))
 
-# Label and entry field for postcode
-tk.Label(root, text="Your Postcode:", font=("Arial", 28)).pack(pady=20)
-postcode_entry = tk.Entry(root, font=("Arial", 24))
-postcode_entry.pack()
+## Label and entry field for postcode
+#tk.Label(root, text="Your Postcode:", font=("Arial", 28)).pack(pady=20)
+#postcode_entry = tk.Entry(root, font=("Arial", 24))
+#postcode_entry.pack()
 
 # Label for mood selection
 tk.Label(root, text="How are you doing?", font=("Arial", 28)).pack(pady=40)
@@ -90,12 +101,12 @@ tk.Label(root, text="How are you doing?", font=("Arial", 28)).pack(pady=40)
 # tk.Button(button_frame, text="😊 Good", font=("Arial", 32), bg="green", fg="white", 
 #           command=lambda: save_response("Good"), width=10, height=3,).pack(side=tk.LEFT, expand=True, fill=tk.BOTH, padx=10,)
 # Create canvas for circular buttons
-canvas = tk.Canvas(root, width=820, height=220)
+canvas = tk.Canvas(root, width=1120, height=220)
 canvas.pack()
 create_circle_button(canvas, 110, 110, 100, "red", "😢 Bad", lambda: save_response("Bad"),"white")
 create_circle_button(canvas, 410, 110, 100, "yellow", "😐 Ok", lambda: save_response("Ok"), "black")
 create_circle_button(canvas, 710, 110, 100, "green", "😊 Good", lambda: save_response("Good"),"white")
-
+create_circle_button(canvas, 1010, 110, 100, "",r"QR_code.png", open_feedback_link,"white")
 # # Log viewer/editor
 # tk.Label(root, text="Responses", font=("Arial", 20)).pack(pady=20)
 # log_text = tk.Text(root, font=("Arial", 16), height=10, width=80, wrap=tk.WORD, state=tk.DISABLED)
